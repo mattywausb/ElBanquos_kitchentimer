4e1804003eed9b0ad3d5c9a96c3f3a9122de1844
Matthias Wegner <19684192+mattywausb@users.noreply.github.com>
2018-08-28 19:50:42 +0200

    Red green LED POC

--- Arduino/led_and_button_workbench/led_and_button_workbench.ino
+++ Arduino/led_and_button_workbench/led_and_button_workbench.ino
@@ -1,16 +1,13 @@
 
-#define LED_R_PIN 3
-#define LED_G_PIN 5
-#define LED_B_PIN 6
+#define LED_R_PIN 5
+#define LED_G_PIN 4
 
-#define BUTTON_A_PIN 2
+#define BUTTON_A_PIN 3
 #define BUTTON_PRESSED LOW
 
-//#define TRACE_ON
+#define TRACE_ON
 
-int channel_R=0;
-int channel_G=0;
-int channel_B=0;
+byte state=0;
 
 void setup() {
   #ifdef TRACE_ON 
@@ -19,25 +16,23 @@ void setup() {
     Serial.println(compile_signature); 
   #endif
 
-  pinMode(LED_R_PIN,OUTPUT);
-  pinMode(LED_G_PIN,OUTPUT);
-  pinMode(LED_B_PIN,OUTPUT);
+  pinMode(LED_R_PIN,OUTPUT);digitalWrite(LED_R_PIN,LOW);
+  pinMode(LED_G_PIN,OUTPUT);digitalWrite(LED_G_PIN,LOW);
   pinMode(BUTTON_A_PIN,INPUT_PULLUP);
 
 }
 
 void loop() {
     
-  channel_R=(millis()/4)%256;
-  channel_G=(millis()/16)%256;
-  channel_B=(millis()/64)%256;
-  analogWrite(LED_R_PIN,channel_R);
-  analogWrite(LED_G_PIN,channel_G);
-  analogWrite(LED_B_PIN,channel_B);
-  #ifdef TRACE_ON
-  Serial.print(channel_R);Serial.print(F("|"));
-  Serial.print(channel_G);Serial.print(F("|"));
-  Serial.print(channel_B);Serial.println();
-  #endif
-  delay(100);
+  if(digitalRead(BUTTON_A_PIN)==BUTTON_PRESSED) {
+    if(++state>2) state=0;
+    switch(state) {
+      case 0: digitalWrite(LED_R_PIN,LOW);digitalWrite(LED_G_PIN,LOW);break;
+      case 1: digitalWrite(LED_R_PIN,LOW);digitalWrite(LED_G_PIN,HIGH);break;
+      case 2: digitalWrite(LED_R_PIN,HIGH);digitalWrite(LED_G_PIN,LOW);break;
+    }
+    delay(100);
+    while(digitalRead(BUTTON_A_PIN)==BUTTON_PRESSED) delay(10);  // Wait until button release
+    delay(100);
+  }
 }
---
+++ Arduino/led_rgb_poc/led_rgb_poc.ino
@@ -0,0 +1,43 @@
+
+#define LED_R_PIN 3
+#define LED_G_PIN 5
+#define LED_B_PIN 6
+
+#define BUTTON_A_PIN 2
+#define BUTTON_PRESSED LOW
+
+//#define TRACE_ON
+
+int channel_R=0;
+int channel_G=0;
+int channel_B=0;
+
+void setup() {
+  #ifdef TRACE_ON 
+    char compile_signature[] = "--- START (Build: " __DATE__ " " __TIME__ ") ---";   
+    Serial.begin(9600);
+    Serial.println(compile_signature); 
+  #endif
+
+  pinMode(LED_R_PIN,OUTPUT);
+  pinMode(LED_G_PIN,OUTPUT);
+  pinMode(LED_B_PIN,OUTPUT);
+  pinMode(BUTTON_A_PIN,INPUT_PULLUP);
+
+}
+
+void loop() {
+    
+  channel_R=(millis()/4)%256;
+  channel_G=(millis()/5+128)%256;
+  channel_B=(millis()/3+64)%256;
+  analogWrite(LED_R_PIN,channel_R);
+  analogWrite(LED_G_PIN,channel_G);
+  analogWrite(LED_B_PIN,channel_B);
+  #ifdef TRACE_ON
+  Serial.print(channel_R);Serial.print(F("|"));
+  Serial.print(channel_G);Serial.print(F("|"));
+  Serial.print(channel_B);Serial.println();
+  #endif
+  delay(100);
+}
