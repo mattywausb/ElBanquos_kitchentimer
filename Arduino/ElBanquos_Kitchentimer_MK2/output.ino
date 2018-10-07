5635fadccf2a8af0d4e37024a7200f832f4d1a9e
Matthias Wegner <19684192+mattywausb@users.noreply.github.com>
2018-10-07 14:51:45 +0200

    turned led display upside down

--- Arduino/ElBanquos_Kitchentimer_MK2/output.ino
+++ Arduino/ElBanquos_Kitchentimer_MK2/output.ino
@@ -12,7 +12,7 @@
 #define LED7SEG_LOAD_PIN 7
 #define LED7SEG_CLK_PIN 6
 
-LedControl led7seg=LedControl(LED7SEG_DATA_PIN,LED7SEG_CLK_PIN,LED7SEG_LOAD_PIN,1);
+LedControl led7seg=LedControl(LED7SEG_DATA_PIN,LED7SEG_CLK_PIN,LED7SEG_LOAD_PIN,1,1);
 
 #define DISPLAY_ACTIVE true
 #define DISPLAY_INTENSITY 1
@@ -241,7 +241,7 @@ void renderTimerCompact(KitchenTimer myKitchenTimer, byte timerSlot) {
       if ( myKitchenTimer.getTimeLeft()<TIMER_CALM_DOWN_TIME || BLINKSTATE(BLINKCYCLE_OVER) ) 
       {
 
-       led7seg.setString(0,timerSlot*2+1,"__",B10000000);   
+       led7seg.setString(0,timerSlot*2+1,"__",B01);   
        return;        
       }
    }
@@ -257,34 +257,34 @@ void renderTimeCompact(byte timerSlot,long currentTime,bool dotState)
    if(currentTime>=86400)  // day display mode
    { 
       sprintf(s, "%dd", currentTime/86400);
-      led7seg.setString(0,startSegment,s,dotState?B01000000:0);    
+      led7seg.setString(0,startSegment,s,dotState?B00000000:0);    
       return;  
    }
    
    if(currentTime>=36000)  // 10 Hour display mode
    { 
       sprintf(s, "H%d", currentTime/36000);
-      led7seg.setString(0,startSegment,s,dotState?B01000000:0);    
+      led7seg.setString(0,startSegment,s,dotState?B00000000:0);    
       return;  
    }
    
    if(currentTime>=3600)  // Hour display mode
    { 
       sprintf(s, "%dh", currentTime/3600);
-      led7seg.setString(0,startSegment,s,dotState?B01000000:0);     
+      led7seg.setString(0,startSegment,s,dotState?B00000000:0);     
       return;  
    }
    
    if(currentTime>=600)  // Minute display mode
    { 
-      led7seg.setDigit(0,startSegment,currentTime/600,false);
-      led7seg.setDigit(0,startSegment-1,(currentTime/60)%10,dotState);
+      led7seg.setDigit(0,startSegment,currentTime/600,dotState);
+      led7seg.setDigit(0,startSegment-1,(currentTime/60)%10,false);
       return;  
    }
 
    if(currentTime>59) {  //Minute + 10 second display mode
     led7seg.setDigit(0,startSegment,currentTime/60, dotState);
-    led7seg.setDigit(0,startSegment-1,(currentTime%60)/10,false);
+    led7seg.setDigit(0,startSegment-1,(currentTime%60)/10,dotState);
     return;
    }
    
@@ -300,13 +300,13 @@ void renderTimeLong(long currentTime)
    currentTime=abs(currentTime);
    if(currentTime<86400)  {// below 24 Hour display mode hh"h"mm.ss
       sprintf(s, "%2ldh%02ld%02ld", (currentTime/3600)%24,(currentTime/60)%60,currentTime%60);
-      led7seg.setString(0,6,s,B00001000);    
+      led7seg.setString(0,6,s,B00010100);    
         return;  
    }
 
    // more then 24 Hour display mode d"d"hh"h"mm
    sprintf(s, "%ldd%2ldh%02ld", (currentTime/86400)%24,(currentTime/3600)%24,(currentTime/60)%60);
-   led7seg.setString(0,6,s,B00000010);    
+   led7seg.setString(0,6,s,B00000100);    
    return;  
 
 }
